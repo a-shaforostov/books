a49244433bf665e80afcd1de00e1b7c2015b83ad
andrii.shaforostov <andpr99@gmail.com>
2018-06-03 21:39:25 +0300

    unplug cerebral debugger

--- src/controller.js
+++ src/controller.js
@@ -3,25 +3,29 @@
  * @module controller
  */
 
+// Uncomment lines to attach cerebral debugger
+// Commented to avoid console errors in development mode
 
 import { Controller } from 'cerebral';
-import Devtools from 'cerebral/devtools';
+// import Devtools from 'cerebral/devtools';
 
 import app from './app';
 
 const controller = Controller(app, {
-  devtools: navigator.userAgent.toLowerCase().includes('chrome')
-    ? Devtools({
-      host: 'localhost:8686',
-      bigComponentsWarning: 20,
-    })
-    : null
+  devtools: null,
+  // devtools: navigator.userAgent.toLowerCase().includes('chrome')
+  //   ? Devtools({
+  //     host: 'localhost:8686',
+  //     bigComponentsWarning: 20,
+  //   })
+  //   : null
 });
 
 // try to login silently (w/o login form)
 const autologin = controller.getSignal('autologin');
 autologin({ silent: true });
 
+// start chat conversation with greeting message
 const startConversation = controller.getSignal('publicModule.startStep');
 startConversation();
 
